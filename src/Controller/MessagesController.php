61258539aca7e032d3dcdf7b8dbe342349eb30d4
sap19 <stevenportillo19@hotmail.com>
2020-04-07 09:57:54 -0400

    Message Get
    Set up get fucntion for api. We are allowed to get messages from a certain thread now.

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Event\Event;
 use Cake\Http\Exception\NotFoundException;
-
-
+use Cake\ORM\Query;
+use Cake\ORM\Table;
+use Cake\ORM\Entity;
+use Cake\ORM\ResultSet;
 
 class MessagesController extends AppController
 {
@@ -14,9 +16,24 @@ class MessagesController extends AppController
     {
         parent::initialize();
         $this->loadComponent('RequestHandler');
+        $this->Auth->allow(["index"]);
+    }
+    public function index($id)
+    {
+        $query = $this->Messages->find('all')->where(['thread_id' => $id]);
+        //$message = $this->paginate($this->Messages->getAlias($this->Threads));
+        //$message = $this->Messages->thread_id->get($thread_id);
+        //$query = $this->Messages->find('all');
+        //$query = $this->Messages->find('ownedBy', ['thread' => $id]);
+        //dd($query);
+        //$row = $query->first();
+       
+        $this->set([
+            'Messages' => $query,
+            '_serialize' => ['Messages']
+        ]);
         
     }
-
     public function add()
     {
         $message = $this->Messages->newEntity();
